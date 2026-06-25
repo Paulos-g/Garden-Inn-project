6c40768b879301a7e3601299fac54164b1141496
Paulos <paulosgirmachew28@gmail.com>
2026-06-25 16:20:19 +0300

    Added AOs animation to room page

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { use } from "react";
 import Header from "../components/Headers";
 import Heading from "../components/Heading";
 import Nav from "../components/nav-bar";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 function Room() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+  }, []);
   return (
     <div>
       <Nav />
-      <Heading
-        sTtitle="GARDEN INN ADDIS ABABA"
-        bTitle="Rooms & Suits Designed for You"
-        descrp={
-          <>
-            From inttimate single rooms to signature garden suits <br /> Every
-            rooms crafted for your comfort{" "}
-          </>
-        }
-      />
-      ;{" "}
+      <div data-aos="fade-up">
+        <Heading
+          sTtitle="GARDEN INN ADDIS ABABA"
+          bTitle="Rooms & Suits Designed for You"
+          descrp={
+            <>
+              From inttimate single rooms to signature garden suits <br /> Every
+              rooms crafted for your comfort{" "}
+            </>
+          }
+        />
+      </div>
     </div>
   );
 }
