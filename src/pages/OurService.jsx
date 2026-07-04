877ac3f302a0e8935e341928ec357800d0f63162
Paulos <paulosgirmachew28@gmail.com>
2026-07-05 00:01:46 +0300

    Add AOS Animation to service page

--- src/pages/OurService.jsx
+++ src/pages/OurService.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import Heading from "../components/Heading";
 import Nav from "../components/nav-bar";
+import AOS from "aos";
+
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function OurService() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+  }, []);
   return (
     <div>
       <Nav />
-      <Heading
-        sTitle="Garden Inn · Addis Ababa"
-        type="content-container"
-        bTitle="Exceptional Services At Every Turn"
-        buttonText="Order Now"
-        descrp="From our craft brewery to world-class wellness, every service at Garden Inn is crafted to make your stay unforgettable."
-      />
+      <div data-aos="fade-up">
+        <Heading
+          sTitle="Garden Inn · Addis Ababa"
+          type="content-container"
+          bTitle="Exceptional Services At Every Turn"
+          buttonText="Order Now"
+          descrp="From our craft brewery to world-class wellness, every service at Garden Inn is crafted to make your stay unforgettable."
+        />
+      </div>
     </div>
   );
 }
