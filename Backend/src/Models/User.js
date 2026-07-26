0d5b02aa5ee6f340b34d2d7bf9531d5d8029dbf9
Paulos <paulosgirmachew28@gmail.com>
2026-07-26 05:59:57 +0300

    Created user model

---
+++ Backend/src/Models/User.js
@@ -0,0 +1,61 @@
+// models/User.js
+import mongoose  from "mongoose";
+
+// 1.create a schema 
+// 2.Model based on the schema u created 
+
+const Userschema = new mongoose.Schema({
+   
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8
+    },
+
+    role: {
+      type: String,
+      enum: ["guest", "admin", "staff"],
+      default: "guest"
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+    
+})
