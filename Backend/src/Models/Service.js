f443132c477eb8ae4d703f9bc4a8891ecd7759aa
Paulos <paulosgirmachew28@gmail.com>
2026-07-28 07:45:37 +0300

    Created User model and stored in DB

---
+++ Backend/src/Models/Service.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const serviceSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    serviceName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    guest: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const Service = mongoose.model("Service", serviceSchema);
